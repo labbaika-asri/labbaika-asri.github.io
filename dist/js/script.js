1361d6cc3e43e9fd8a5487890fc8d47381b6bdc6
Labbaika Asri <labbaikaasri07@gmail.com>
2022-12-21 20:19:48 +0700

    memperbaiki navbar, menambahkan particle.js dan mengatur kembali posisi back to top

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,10 +5,10 @@ window.onscroll = function () {
   const toTop = document.querySelector("#to-top");
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
-    toTop.classList.remove("translate-y-20");
+    toTop.classList.remove("translate-y-32");
   } else {
     header.classList.remove("navbar-fixed");
-    toTop.classList.add("translate-y-20");
+    toTop.classList.add("translate-y-32");
   }
 };
 
@@ -52,3 +52,115 @@ if (
   document.documentElement.classList.remove("dark");
   darkMode.checked = false;
 }
+
+// Particles JS
+particlesJS({
+  particles: {
+    number: {
+      value: 110,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#334155",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 250,
+        height: 150,
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 8,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#64748b",
+      opacity: 0.4,
+      width: 2,
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
+      },
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
+});
